658a8fd6cb234edd4f39ac241f2984c434e6dc78
bcavs <cavenagh.ben@gmail.com>
2020-10-16 22:56:11 -0500

    Add auth screens

--- App.js
+++ App.js
@@ -2,11 +2,19 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+import HomeScreen from "./src/screens/HomeScreen";
+import LoadingScreen from "./src/screens/LoadingScreen";
+import LoginScreen from "./src/screens/LoginScreen";
+import SignUpScreen from "./src/screens/SignUpScreen";
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>HELLO DUNBAR!</Text>
       <StatusBar style="dark" />
+      {/* <HomeScreen /> */}
+      {/* <LoadingScreen /> */}
+      <LoginScreen />
+      {/* <SignUpScreen /> */}
     </View>
   );
 }
