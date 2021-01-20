c7eae0cef9c90e905c4a9f940b0f1ade0ce4b703
bcavs <cavenagh.ben@gmail.com>
2021-01-19 23:12:18 -0600

    Create user on register
    Updating the register function to create a user in the "users" collection when they sign up. The userData is added to the collection under the auto-generated uid that firebase gives us -- will make fetching that specific user easier

--- src/helpers/authHelpers.js
+++ src/helpers/authHelpers.js
@@ -1,4 +1,5 @@
 import firebase from "firebase";
+import 'firebase/firestore';
 
 export async function login(email, password) {
   try {
@@ -8,9 +9,48 @@ export async function login(email, password) {
   }
 }
 
-export async function register(email, password) {
+/**
+ * Registers a user in Firebase 
+ * @param {Object} user -- the user object
+ * @param {String} user.firstName -- first name of the user object
+ * @param {String} user.lastName -- last name of the user object
+ * @param {String} user.email -- email of the user object
+ * @param {String} user.password -- password of the user object
+ */
+export async function register({ email, password, firstName, lastName }) {
+  //TODO Check that the user object is structured correctly before creating it in Firebase
   try {
-    await firebase.auth().createUserWithEmailAndPassword(email, password);
+    await 
+    firebase.auth()
+      .createUserWithEmailAndPassword(email,password)
+      .then( (response) => {
+        //Get the uid Firebase assigned the new user
+        const uid = response.user.uid
+        //Create the user object to be saved in Firestore
+        const userData = {
+          id: uid,
+          email: email,
+          firstName: firstName,
+          lastName: lastName,
+        }
+
+        //Get the firestore reference of the "users" collection
+        const usersRef = firebase.firestore().collection("users")
+        console.log("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+        console.log("Adding this user object to the Firestore: ", userData)
+        console.log("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+
+        //Add the user to the "users" collection with the auto-generated uid as the key
+        usersRef
+          .doc(uid)
+          .set(userData)
+          .then(() => console.log("User has been added to Firebase"))
+          .catch(error => alert(error))
+        }
+      )
+      .catch(error => 
+        alert(error)
+      );
   } catch (e) {
     console.log("Error: ", e);
     // setErrorMessage(e);
